e04ed554c161bc9f1f6489944669de1ee6b6a648
Tony Aldridge <zaragopha@hotmail.com>
2012-09-17 13:57:06 +0100

    Began blocking out basic Engine code

--- src/basics/basics.rs
+++ src/basics/basics.rs
@@ -1,6 +1,46 @@
 use sdl;
 
+struct Engine {
+    running:bool 
+}
+
+impl Engine {
+    fn on_execute() -> int {
+        if (self.on_init() == false) {
+            return -1;
+        }
+
+        while (self.running) {
+
+            self.on_loop();
+            self.on_render();
+        }
+
+        self.on_cleanup();
+        return 0;
+    }
+
+    fn on_init() -> bool {
+        return false;
+    }
+
+    fn on_loop() {
+    }
+
+    fn on_render() {
+    }
+
+    fn on_cleanup() {
+    }
+}
+
+fn Engine() -> Engine {
+    return Engine {
+        running: true 
+    };
+}
+
 fn main() {
-    //TODO: Implement
-    io::println("Not yet implemented");
+    let engine = Engine();
+    engine.on_execute();
 }
