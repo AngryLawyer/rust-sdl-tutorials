09d5bd00e60063bf428002f17687a88e537c0da2
Tony Aldridge <zaragopha@hotmail.com>
2012-09-19 13:56:32 +0100

    Switched Option for Result, so we can get errors out

--- src/basics/basics.rs
+++ src/basics/basics.rs
@@ -9,6 +9,7 @@ impl Engine {
     fn on_execute() -> int {
 
         while (self.running) {
+            //Handle the event poll 
             let mut polling = true;
             while polling {
                 sdl::event::poll_event(|event| {
@@ -27,31 +28,44 @@ impl Engine {
         return 0;
     }
 
+    /*
+     * Handles game logic, which is nothing at the moment
+     */
     fn on_loop() {
     }
 
+    /*
+     * Handles rendering logic, which is also nothing!
+     */
     fn on_render() {
     }
 
+    /*
+     * It's always good practice to release all the things we've aquired
+     * before quitting out
+     */
     fn on_cleanup() {
+        sdl::video::free_surface(self.surface);
         sdl::quit();
     }
 }
 
-fn Engine() -> option::Option<Engine> {
-    //This guy does initialization these days
-    //Try to create the surface
-    if sdl::init(~[sdl::InitEverything]) < 0 {
-        return option::None;
+/*
+ * Construct our Engine, and fill it with the surface we're going to need
+ */
+fn Engine() -> result::Result<Engine, ~str> {
+
+    if sdl::init(~[sdl::InitVideo]) < 0 {
+        return result::Err(#fmt("Unable to initialize SDL: %s", sdl::get_error()));
     }
 
     let surface = sdl::video::set_video_mode(640, 480, 31, ~[sdl::video::HWSurface], ~[sdl::video::DoubleBuf]);
 
     if surface == ptr::null() {
-        return option::None;
+        return result::Err(#fmt("Unable to create surface: %s", sdl::get_error()));
     }
 
-    return option::Some(Engine {
+    return result::Ok(Engine {
         running: true,
         surface: surface
     });
@@ -59,9 +73,9 @@ fn Engine() -> option::Option<Engine> {
 
 fn main() {
     let maybe_engine = Engine();
-    if option::is_some(maybe_engine) {
-        option::unwrap(maybe_engine).on_execute();
+    if result::is_ok(maybe_engine) {
+        result::unwrap(maybe_engine).on_execute();
     } else {
-        io::println("Error - couldn't initialize the Engine!");
+        io::println(result::unwrap_err(maybe_engine));
     }
 }
